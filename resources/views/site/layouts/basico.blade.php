f23338fbdd29c35556dc885892dc5c1f4d50520a
jrconsta <jrconsta29@gmail.com>
2020-12-09 22:52:11 -0400

    Versão 1
    Aula 61

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+
+        <link rel="stylesheet" href=" {{asset('estilos.css')}} ">
+        
+    </head>
+
+    <body>
+        @yield('conteudo')
+    </body>
+</html>
