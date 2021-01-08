d5f90f156ba9f7a9401b5aa5e62ed84874372023
jrconsta <jrconsta29@gmail.com>
2021-01-07 23:50:41 -0400

    até aula 113

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
-{
-    //
+{   
+    protected $table = 'site_contatos';
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo', 'mensagem', 'created_at', 'updated_at'];
 }
